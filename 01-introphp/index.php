a85497147ea36dc775f070b1073c24a895c20694
rosto-infinity <rostoinfocus@gmail.com>
2026-03-25 16:03:03 +0100

    cours php 2026

---
+++ 01-introphp/index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma première page PHP</title>
+</head>
+<body>
+    <h1><?php echo "Bienvenue sur ma page PHP !"; ?></h1>
+    <p>Nous sommes en l'an :
+       <?php echo date("Y"); ?></p>
+</body>
+</html>
+
