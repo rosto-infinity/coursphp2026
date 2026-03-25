ba974e2a7cae656bfb958fcaca6a3bbfce56691e
rosto-infinity <rostoinfocus@gmail.com>
2026-03-25 16:11:25 +0100

    Bienvenue sur ma page PHP

--- 01-introphp/index.php
+++ 01-introphp/index.php
@@ -1,13 +1,17 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Ma première page PHP</title>
 </head>
+
 <body>
     <h1><?php echo "Bienvenue sur ma page PHP !"; ?></h1>
     <p>Nous sommes en l'an :
-       <?php echo date("Y"); ?></p>
+        <?php echo date("Y"); ?>
+    </p>
+    <p>Heure actuelle : <?= date("H:i:s") ?></p>
+<!-- Équivalent à : <?php echo date("H:i:s"); ?> -->
 </body>
-</html>
 
+</html>
