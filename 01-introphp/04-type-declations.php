059849639ac3e7708ed07daea55ac9e27303aebc
rosto-infinity <rostoinfocus@gmail.com>
2026-03-26 10:51:51 +0100

    # PHP 8.0+)

--- 01-introphp/04-type-declations.php
+++ 01-introphp/04-type-declations.php
@@ -14,3 +14,24 @@ function saluer(string $nom): void {
 saluer("Alice "); // Fonctionne
 
 #saluer(123);     // TypeError !
+
+#Déclarations de Type de Retour
+function additionner(int $a, int $b): int {
+    return $a + $b;
+}
+
+// function trouverUtilisateur(int $id): ?User { // ? permet null
+//     return $id > 0 ? new User() : null;
+// }
+
+function journaliser(string $msg): void { // Pas de retour
+    echo $msg;
+}
+
+# PHP 8.0+)
+function traiterID(int|string $id): void {
+    echo "Traitement : $id <br>";
+}
+
+traiterID(42);        // Fonctionne
+traiterID("ABC123");  // Fonctionne aussi
